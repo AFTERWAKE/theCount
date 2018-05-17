5ce39f9a58783299437cc6b9c8464191edad6698
Noah Siano <nsiano@bellsouth.net>
2018-05-17 17:23:07 -0500

    add auth redirect

--- bot.js
+++ bot.js
@@ -35,6 +35,27 @@ var bot_options = {
     scopes: ['bot']
 };
 
+app.get('/auth/redirect', (req, res) =>{
+    var options = {
+        uri: 'https://slack.com/api/oauth.access?code='
+            +req.query.code+
+            '&client_id='+process.env.CLIENT_ID+
+            '&client_secret='+process.env.CLIENT_SECRET+
+            '&redirect_uri='+process.env.REDIRECT_URI,
+        method: 'GET'
+    }
+    request(options, (error, response, body) => {
+        var JSONresponse = JSON.parse(body)
+        if (!JSONresponse.ok){
+            console.log(JSONresponse)
+            res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
+        }else{
+            console.log(JSONresponse)
+            res.send("Success!")
+        }
+    })
+})
+
 bot_options.json_file_store = __dirname + '/.data/db/'; // store user data in a simple JSON format
 
 // Create the Botkit controller, which controls all instances of the bot.
